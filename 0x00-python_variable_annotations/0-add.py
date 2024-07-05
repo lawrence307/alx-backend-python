9cfc2c8f94e11019b71a2a90d661d55efadee513
lawrence307 <lawrence_muthini@yahoo.com>
2024-07-05 10:54:47 +0000

    updates

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+""" a module that contains a single function that adds two integers """
 def add(a: float, b: float) -> float:
+    """Add two integers together."""
     return a + b
 
--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,4 +3,10 @@
 from typing import List
 
 def sum_list(input_list: List[float]) -> float:
+    """Sums a list of floats
+    Args:
+        input_list (list): A list of floats
+    Returns:
+        float: The sum of the floats in the list
+    """
     return sum(input_list)
